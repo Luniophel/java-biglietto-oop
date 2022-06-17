93be1f20fffe67a927c616d4653f2569e01d2b10
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-17 16:12:12 +0200

    add Biglietto() and Biglietteria, add methods to calculate ticket price, add print results

--- src/jana60/Biglietteria.java
+++ src/jana60/Biglietteria.java
@@ -1,9 +1,37 @@
 package jana60;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Biglietteria {
+	
+	//Ho dimenticato di committare, chiedo scusa.. mi ero immerso nel testare alcune cose
 
 	public static void main(String[] args) {
-
+		
+		//Dichiarazione delle classi che verranno utilizzate
+		Scanner scan = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("##.00€");
+		
+		System.out.println("Grazie per aver scelto Generation Rails per il vostro viaggio!");
+		
+		//Inizializzazione del biglietto
+		Biglietto ticket = new Biglietto();
+		
+		//Richiesta dell'età dell'utente
+		System.out.print("Prego, inserire l'età dell'utente che vuole acquistare il biglietto: ");
+		ticket.setPassengerAge(Float.parseFloat(scan.nextLine()));
+		
+		//Richiesta della lunghezza del tragitto
+		System.out.print("\nInserire la lunghezza del tragitto: ");
+		ticket.setTripKm(Float.parseFloat(scan.nextLine()));
+		
+		//Stampa del prezzo calcolato
+		System.out.print("Questo è il prezzo per il biglietto selezionato: " + df.format(ticket.calcTicketPrice()));
+		
+		
+		scan.close();
+		
 	}
 
 }
--- src/jana60/Biglietto.java
+++ src/jana60/Biglietto.java
@@ -1,5 +1,54 @@
 package jana60;
 
 public class Biglietto {
+	//ATTRIBUTES
+	//Gli attributi che dovranno essere riempiti dall'utente
+	private float tripKm, passengerAge;
+	
+	//Le tariffe che mi vengono fornite dalla compagnia di treni
+	private float pricePerKm = 0.21f;		// 0,21 €/km
+	private float childrenDiscount = 0.20f;	// Sconto del 20% per i minorenni
+	private float elderDiscount = 0.40f;	// Sconto del 40% per gli over65
+	
+	//CONSTRUCTORS
+	//Costruttore del biglietto
+	public Biglietto(float tripKm, int passengerAge){
+		
+		this.tripKm = tripKm;
+		this.passengerAge = passengerAge;
+		
+	}
+	public Biglietto() {}	//Costruttore che permette l'inizializzazione vuota di un biglietto
+	
+	//SETTERS/GETTERS
+	public void setTripKm(float tripKm) {
+		
+		this.tripKm = tripKm;
+	}
+	public void setPassengerAge(float passengerAge) {
+		
+		this.passengerAge = passengerAge;
+		
+	}
+	
+	//METHODS
+	public float calcTicketPrice(){
+		
+		//Calcolo del prezzo base utilizzando la lunghezza del percorso inserita dall'utente e la tariffa data dalla compagnia
+		float ticketPrice = tripKm * pricePerKm;
+		//Applicazione dello sconto per minorenni
+		if (passengerAge < 18) {
+			ticketPrice -= (ticketPrice * childrenDiscount);
+		} 
+		//Applicazione dello sconto per over65
+		if (passengerAge >= 65) {
+			ticketPrice -= (ticketPrice * elderDiscount);
+		} 
+		return ticketPrice;
+		
+	}
+
+	
+	
 
 }
